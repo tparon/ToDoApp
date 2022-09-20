642f94a791fe9904690275bd5b2cc245a23cf49d
Aron Toth-Peli <aron.tothpeli@accretivetg.com>
2022-09-20 12:04:09 +0200

    message handling

--- main.js
+++ main.js
@@ -5,15 +5,54 @@ function onSubmit(event) {
     create(task.value, date.value)
 }
 
-let timeout;
+let myDiv = []
+let myButton = []
 
 function create(text, date) {
     const divElem = document.createElement("div")
     divElem.innerText = date + ": " + text
-    document.body.appendChild(divElem)
+    myDiv.push(divElem)
 
+    const buttonElem = document.createElement("button")
+    buttonElem.innerText = "Delete"
+    myButton.push(buttonElem)
+
+
+    console.log(myDiv)
+    console.log(myButton)
+
+    for (let i = 0; i < myDiv.length; i++) {
+        if (myDiv[i] != undefined) {
+            document.body.appendChild(myDiv[i])
+            document.body.appendChild(myButton[i])
+        }
+    }
+
+    deleteLine()
+    messageHandler("Sikeresen hozzá adva!")
+}
+
+function deleteLine() {
+    for (let i = 0; i < myButton.length; i++) {
+        myButton[i].onclick = function () {
+            for( let i = 0; i < myDiv.length; i++){
+                if ( myDiv[i] != undefined) {
+                    myDiv.splice(i, 1)
+                    myButton.splice(i, 1)
+                    myDiv[i].remove()
+                }
+            }
+        }
+    }
+
+    messageHandler("Sikeresen törölve!")
+}
+
+let timeout
+
+function messageHandler(message) {
     const succesMessage = document.getElementById("success-message")
-    succesMessage.innerText = "Success!"
+    succesMessage.innerText = message
 
     clearTimeout(timeout)
 
